53c8cd26fa537a8bb17bedc0ac00678a029991c6
TheMedina <37382554+TheMedina@users.noreply.github.com>
2018-04-04 22:54:41 -0400

    bash uploads
    uploaded more bash files

---
+++ bash_scripts/deploy_static_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+STAGE="${1:-dev}"
+echo "Deploying static assets to ${STAGE}..."
+
+BUCKET_NAME=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "news-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='WebSiteBucket'] | [0].OutputValue" \
+    --output text)
+
+WEBSITE_URL=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "news-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='WebSiteUrl'] | [0].OutputValue" \
+    --output text)
+
+aws s3 sync --acl 'public-read' --delete ./static/ "s3://${BUCKET_NAME}/"
+
+echo "Bucket URL: ${WEBSITE_URL}"
